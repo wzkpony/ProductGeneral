29069df7fcc18d8e9fa6a91bcad1d78140a11c7f
wzk <wzk@sanlincaifudeiMac.local>
2017-06-14 12:42:08 +0800

    添加基类

---
+++ PodProduct/BaseUI/BaseTextField.h
@@ -0,0 +1,13 @@
+//
+//  BaseTextField.h
+//  PodProduct
+//
+//  Created by wzk on 2017/6/14.
+//  Copyright © 2017年 wzk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseTextField : UITextField
+
+@end
---
+++ PodProduct/BaseUI/BaseTextView.m
@@ -0,0 +1,21 @@
+//
+//  BaseTextView.m
+//  PodProduct
+//
+//  Created by wzk on 2017/6/14.
+//  Copyright © 2017年 wzk. All rights reserved.
+//
+
+#import "BaseTextView.h"
+
+@implementation BaseTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
